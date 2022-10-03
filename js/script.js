fb865b66267a3f5b45b930b477a3ea5282582c35
OlegNestayko <oleg_nestayko@ukr.net>
2022-10-04 00:39:07 +0300

    Replaced swiper slider with slick

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+// Swiper slider
 new Swiper('.swiper', {
   // Optional parameters
   loop: true,
@@ -14,9 +15,43 @@ new Swiper('.swiper', {
     prevEl: '.swiper-button-prev',
   },
 
+  slidesPerView: 3,
+
+  // effect: 'coverflow',
+  // coverflowEffect: {
+  //   rotate: 30,
+  //   slideShadows: false,
+  // },
+
   // Slides effect
-  effect: 'flip',
-  flipEffect: {
-    slideShadows: false,
-  },
+  // effect: 'flip',
+  // flipEffect: {
+  //   slideShadows: false,
+  // },
+});
+
+// const swiperGallery = new Swiper('.gallery-container ', {
+//   loop: true,
+
+//   // If we need pagination
+//   pagination: {
+//     el: '.swiper-pagination',
+//     clickable: true,
+//   },
+
+//   // Navigation arrows
+//   navigation: {
+//     nextEl: '.swiper-button-next',
+//     prevEl: '.swiper-button-prev',
+//   },
+// });
+
+// Slick slider
+$(document).ready(function () {
+  $('.gallery__wrapper').slick({
+    // Кол-во фото для показа
+    slidesToShow: 3,
+    dots: true,
+    centerMode: true,
+  });
 });
